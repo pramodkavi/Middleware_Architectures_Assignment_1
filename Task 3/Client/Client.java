6a75c90e206ecc76527b2f6545b77c550b8938f3
Sneha Dissanakaye <2020cs045@stu.ucsc.cmb.ac.lk>
2023-07-22 14:05:25 +0530

    task 3 completed

--- Task 3/Client/Client.java
+++ Task 3/Client/Client.java
@@ -3,20 +3,24 @@ import java.net.*;
 
 public class Client {
     public static void main(String[] args) {
+        // Check if the correct number of arguments is provided
         if (args.length != 4) {
             System.out.println("Usage: java Client <serverIP> <port> <clientType> <topic>");
             return;
         }
 
+        // Parse command-line arguments
         String serverIP = args[0];
         int port = Integer.parseInt(args[1]);
         String clientType = args[2];
         String topic = args[3];
 
         try {
+            // Establish a connection to the server using the specified server IP and port
             Socket socket = new Socket(serverIP, port);
             System.out.println("Connected to server: " + serverIP + ":" + port);
 
+            // Initialize input and output streams for communication with the server
             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
@@ -25,24 +29,31 @@ public class Client {
             writer.println(topic);
 
             if (clientType.equalsIgnoreCase("PUBLISHER")) {
+                // Client is a PUBLISHER
                 BufferedReader keyboardReader = new BufferedReader(new InputStreamReader(System.in));
                 String line;
+                // Read input from the user and send it to the server as messages
                 while ((line = keyboardReader.readLine()) != null) {
                     writer.println(line);
                     if (line.equalsIgnoreCase("terminate")) {
+                        // If the user enters "terminate," stop sending messages
                         break;
                     }
                 }
             } else if (clientType.equalsIgnoreCase("SUBSCRIBER")) {
+                // Client is a SUBSCRIBER
+                // Receive messages from the server and display them
                 while (true) {
                     String message = reader.readLine();
                     if (message == null || message.equalsIgnoreCase("terminate")) {
+                        // If the server sends "terminate," stop receiving messages
                         break;
                     }
                     System.out.println("Received from server: " + message);
                 }
             }
 
+            // Client has finished its task, close the socket and disconnect from the server
             System.out.println("Disconnected from server.");
             socket.close();
         } catch (IOException e) {
--- Task 3/Server/Server.java
+++ Task 3/Server/Server.java
@@ -15,22 +15,28 @@ public class Server {
         int port = Integer.parseInt(args[0]);
 
         try {
+            // Create a server socket to listen for incoming client connections
             serverSocket = new ServerSocket(port);
             System.out.println("Server started. Listening on port " + port);
 
             while (true) {
+                // Wait for a client to connect and accept the connection
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
 
-                // Create a new client handler thread
+                // Create a new client handler to handle the client's communication
                 ClientHandler clientHandler = new ClientHandler(clientSocket);
                 clientHandlers.add(clientHandler);
+
+                // Start a new thread for the client handler to handle communication with this
+                // client
                 Thread clientThread = new Thread(clientHandler);
                 clientThread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
+            // Close the server socket when done
             if (serverSocket != null) {
                 try {
                     serverSocket.close();
@@ -41,19 +47,23 @@ public class Server {
         }
     }
 
+    // Method to broadcast a message to all clients subscribed to a particular topic
     public static void broadcastMessage(String message, String topic, ClientHandler sender) {
         for (ClientHandler clientHandler : clientHandlers) {
             if (clientHandler != sender && clientHandler.getTopic().equals(topic)) {
+                // Send the message to all subscribers for the specified topic
                 clientHandler.sendMessage(message);
             }
         }
     }
 
+    // Method to remove a client handler when a client disconnects
     public static void removeClient(ClientHandler clientHandler) {
         clientHandlers.remove(clientHandler);
     }
 }
 
+// Class to handle communication with a single client
 class ClientHandler implements Runnable {
     private Socket clientSocket;
     private BufferedReader reader;
@@ -64,15 +74,20 @@ class ClientHandler implements Runnable {
     public ClientHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
         try {
+            // Initialize the input and output streams for communication with the client
             reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             writer = new PrintWriter(clientSocket.getOutputStream(), true);
-            clientType = reader.readLine(); // Read the client type (PUBLISHER or SUBSCRIBER)
-            topic = reader.readLine(); // Read the topic
+
+            // Read the client type (PUBLISHER or SUBSCRIBER) and the topic sent by the
+            // client
+            clientType = reader.readLine();
+            topic = reader.readLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    // Method to get the topic of this client (PUBLISHER or SUBSCRIBER)
     public String getTopic() {
         return topic;
     }
@@ -81,25 +96,32 @@ class ClientHandler implements Runnable {
     public void run() {
         try {
             if (clientType.equalsIgnoreCase("PUBLISHER")) {
+                // Handle communication with a publisher client
                 while (true) {
+                    // Read messages from the publisher and broadcast them to subscribers of the
+                    // topic
                     String message = reader.readLine();
                     if (message == null || message.equalsIgnoreCase("terminate")) {
+                        // If the publisher sends "terminate," stop the communication loop
                         break;
                     }
                     Server.broadcastMessage(message, topic, this);
-                    System.out.println("Client : " + topic + " - " + message);
-
+                    System.out.println("Client: " + topic + " - " + message);
                 }
             } else if (clientType.equalsIgnoreCase("SUBSCRIBER")) {
+                // Handle communication with a subscriber client
                 while (true) {
+                    // Read messages from the publisher and display them for the subscriber
                     String message = reader.readLine();
                     if (message == null || message.equalsIgnoreCase("terminate")) {
+                        // If the subscriber sends "terminate," stop the communication loop
                         break;
                     }
                     System.out.println("Received from publisher: " + message);
                 }
             }
 
+            // Client disconnected, cleanup and close the socket
             System.out.println("Client disconnected: " + clientSocket.getInetAddress().getHostAddress());
             Server.removeClient(this);
             clientSocket.close();
@@ -108,6 +130,7 @@ class ClientHandler implements Runnable {
         }
     }
 
+    // Method to send a message to this client
     public void sendMessage(String message) {
         writer.println(message);
     }
